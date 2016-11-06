3791a7a10060e3557d9258b6d85ee6d9885d14e7
David KO <koman2019@gmail.com>
2016-11-06 14:13:31 +0800

    create simple login and register page function

---
+++ app/js/controller/login.js
@@ -0,0 +1,49 @@
+angular.module('teamform-login-app', ['firebase'])
+.controller('LoginCtrl', ['$scope', '$firebaseObject', '$firebaseArray', 
+    function($scope, $firebaseObject, $firebaseArray) {
+		
+	// Call Firebase initialization code defined in site.js
+	initalizeFirebase();
+	console.log("it is login page!")
+	$scope.email = "";
+	$scope.password = "";
+	$scope.doLogin = function () {
+
+		var email = $scope.email;
+		var password = $scope.password;
+
+		
+		
+		firebase.auth().signInWithEmailAndPassword(email, password).then(function(user) {
+	
+			/* after login */
+			console.log(user);
+			
+			
+			
+		}, function(error) {
+
+		  // Handle Errors here.
+		  var errorCode = error.code;
+		  var errorMessage = error.message;
+		  console.log(errorCode + ": " + errorMessage)
+		  alert("You cannot login successfully!")
+		  
+		});
+		
+			
+	}
+			
+}]);
+
+/*
+	firebase.auth().onAuthStateChanged(function(user) {
+	  if (user) {
+		console.log(user)
+	  } else {
+		// No user is signed in.
+		console.log("YEAH - no user")
+	  }
+
+	});	
+*/
