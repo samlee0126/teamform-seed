3d4415ad3abed1713dbb0e5e2a4a5964a969276d
jackylaucf <jackycflau@gmail.com>
2016-11-10 00:29:07 +0800

    basic event layout, minor change in admin js

---
+++ app/js/controller/event.js
@@ -0,0 +1,4 @@
+angular.module('teamform-event-app', ['firebase'])
+.controller('eventCtrl', ['$scope', '$firebaseObject', '$firebaseArray', '$window', function($scope, $firebaseObject, $firebaseArray, $window) {
+    
+}]);
