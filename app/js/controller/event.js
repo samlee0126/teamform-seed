a7bd0dbe3d45618e62ab124bdc47cee4edbd1c59
jackylaucf <jackycflau@gmail.com>
2016-11-13 20:47:22 +0800

    event page layout refinement

--- app/js/controller/event.js
+++ app/js/controller/event.js
@@ -99,7 +99,10 @@ angular.module('teamform-event-app', ['firebase'])
                 $scope.eventName = data.child("eventName").val();
                 $scope.maxForEachTable = data.child("maxForEachTable").val();
                 $scope.numberOfCurrentTable = data.child("numberOfCurrentTable").val();
-
+                $scope.time = data.child("time").val();
+                $scope.venue = data.child("venue").val();
+                $scope.organizer = data.child("organizer").val();
+                $scope.maxForEachTable = data.child("maxForEachTable").val();
 
                 // update $scope
                 $scope.$apply();
@@ -120,8 +123,49 @@ angular.module('teamform-event-app', ['firebase'])
 
 
 
+function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
 
+function initializeClock(id, endtime) {
+  var clock = document.getElementById(id);
+  var daysSpan = clock.querySelector('.days');
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
+
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+}
+
+var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+initializeClock('clockdiv', deadline);
 
 
 
 }]);
+
